c332e101b6a8cbc9c7eb4864291fbc71339acf3c
Uvashree5 <uvashreesenthilkumar42@gmail.com>
2024-10-08 23:21:47 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request
+from models import generate_recommendations 
+
+app = Flask(__name__)
+
+# Home page route with the "Health Wealth Optimizer" button
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+# Route for the input form page
+@app.route('/input')
+def input_page():
+    return render_template('input.html')
+
+
+
+
+@app.route('/result', methods=['GET' , 'POST'])
+def result_page():
+    if request.method == 'POST':
+    # Collect user inputs from the form
+        age = int(request.form['age'])
+        income = float(request.form['income'])
+        health_expenses = float(request.form['health_expenses'])
+        savings_goal = float(request.form['savings_goal'])
+        current_investment = float(request.form['current_investment'])
+
+        
+    
+    # Call AI model to generate recommendations
+        recommendations = generate_recommendations(age, income, health_expenses, savings_goal, current_investment)
+    
+    # Render the result.html page with the recommendations
+        return render_template('result.html', recommendations=recommendations)
+    else:
+        # Handle GET request (optional)
+        return "You are not supposed to access this page directly."
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
